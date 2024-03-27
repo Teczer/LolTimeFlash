03bbf24d532b7eeeb74293839e69e4f4d34235b1
Teczer <mehdi.hattou1@gmail.com>
2024-03-27 22:23:27 +0100

    :tada: Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,100 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+const leagueRoles = [
+  {
+    name: "TOP",
+    src: "https://ih1.redbubble.net/image.2355783081.2767/st,small,507x507-pad,600x600,f8f8f8.jpg",
+  },
+  {
+    name: "JUNGLE",
+    src: "https://ih1.redbubble.net/image.2354146454.1555/st,small,507x507-pad,600x600,f8f8f8.jpg",
+  },
+  {
+    name: "MID",
+    src: "https://ih1.redbubble.net/image.2354236031.4306/st,small,507x507-pad,600x600,f8f8f8.jpg",
+  },
+  {
+    name: "ADC",
+    src: "https://ih1.redbubble.net/image.2354182272.2636/st,small,507x507-pad,600x600,f8f8f8.jpg",
+  },
+  {
+    name: "SUPPORT",
+    src: "https://ih1.redbubble.net/image.2354270772.5334/st,small,507x507-pad,600x600,f8f8f8.jpg",
+  },
+];
+
 export default function Home() {
+  const [gameStartTime, setGameStartTime] = useState(null);
+  const [timers, setTimers] = useState({});
+
+  const handleStart = () => {
+    setGameStartTime(new Date());
+  };
+
+  const handleClick = (role) => {
+    if (!gameStartTime) {
+      alert("Veuillez d'abord démarrer la partie");
+      return;
+    }
+
+    const now = new Date();
+    const timeElapsedInSeconds = (now - gameStartTime) / 1000;
+    const cooldownEndTime = new Date(
+      now.getTime() + (5 * 60 - timeElapsedInSeconds) * 1000
+    );
+
+    const formattedMinutes = cooldownEndTime
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+    const formattedSeconds = cooldownEndTime
+      .getSeconds()
+      .toString()
+      .padStart(2, "0");
+
+    const countdownText = `${role.toUpperCase()}${formattedMinutes}:${formattedSeconds}`;
+
+    setTimers({ ...timers, [role]: countdownText });
+
+    // Copier dans le presse-papiers
+    navigator.clipboard
+      .writeText(countdownText)
+      .then(() =>
+        console.log("Texte copié dans le presse-papiers :", countdownText)
+      )
+      .catch((err) =>
+        console.error("Erreur lors de la copie dans le presse-papiers :", err)
+      );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col justify-around items-center min-h-screen">
+      <div className="w-full flex justify-around items-center">
+        {leagueRoles.map((role, index) => (
+          <div key={index}>
+            <button
+              className="transition-all hover:scale-110"
+              onClick={() => handleClick(role.name)}
+            >
+              <Image
+                className="w-20 h-20"
+                width={200}
+                height={200}
+                src={role.src}
+                alt={role.name}
+              />
+            </button>
+            {timers[role.name] && <p>{timers[role.name]}</p>}
+          </div>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Button variant="outline" className="w-1/6" onClick={handleStart}>
+        Start
+      </Button>
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ih1.redbubble.net",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 export default nextConfig;
