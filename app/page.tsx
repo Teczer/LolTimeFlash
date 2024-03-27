58680d70c0c3bf0706a309ff30ff2c7c315aa43d
Teczer <mehdi.hattou1@gmail.com>
2024-03-27 23:04:45 +0100

    construction_worker first version

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
-const leagueRoles = [
+interface LeagueRoles {
+  name: string;
+  src: string;
+}
+
+const leagueRoles: LeagueRoles[] = [
   {
     name: "TOP",
     src: "https://ih1.redbubble.net/image.2355783081.2767/st,small,507x507-pad,600x600,f8f8f8.jpg",
@@ -28,73 +34,107 @@ const leagueRoles = [
 ];
 
 export default function Home() {
-  const [gameStartTime, setGameStartTime] = useState(null);
-  const [timers, setTimers] = useState({});
+  const [gameTimer, setGameTimer] = useState<number | null>(null);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
+  const [isSummonerIsTimed, setIsSummonerIsTimed] = useState({
+    TOP: false,
+    JUNGLE: false,
+    MID: false,
+    SUPPORT: false,
+    ADC: false,
+  });
+  const [cooldownTimers, setCooldownTimers] = useState<{
+    [key: string]: string;
+  }>({});
 
-  const handleStart = () => {
-    setGameStartTime(new Date());
-  };
+  function startGame() {
+    setGameTimer(new Date().getTime());
+  }
 
-  const handleClick = (role) => {
-    if (!gameStartTime) {
-      alert("Veuillez d'abord démarrer la partie");
-      return;
+  function startFlashCooldown(role: string) {
+    const startTime = new Date().getTime();
+    const endTime = startTime + 5 * 60 * 1000;
+
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const timeLeft = Math.max(0, Math.floor((endTime - now) / 1000));
+
+      if (timeLeft === 0) {
+        clearInterval(interval);
+        setIsSummonerIsTimed((prevState) => ({ ...prevState, [role]: false }));
+        setCooldownTimers((prevTimers) => ({ ...prevTimers, [role]: "" }));
+        return;
+      }
+
+      const minutes = Math.floor(timeLeft / 60);
+      const seconds = timeLeft % 60;
+      const formattedTime = `${minutes}:${String(seconds).padStart(2, "0")}`;
+      setCooldownTimers((prevTimers) => ({
+        ...prevTimers,
+        [role]: formattedTime,
+      }));
+    }, 1000);
+
+    setIsSummonerIsTimed((prevState) => ({ ...prevState, [role]: true }));
+  }
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (gameTimer !== null) {
+      interval = setInterval(() => {
+        const now = new Date().getTime();
+        const timeElapsed = Math.floor((now - gameTimer) / 1000);
+        setElapsedTime(timeElapsed);
+      }, 1000);
     }
 
-    const now = new Date();
-    const timeElapsedInSeconds = (now - gameStartTime) / 1000;
-    const cooldownEndTime = new Date(
-      now.getTime() + (5 * 60 - timeElapsedInSeconds) * 1000
-    );
-
-    const formattedMinutes = cooldownEndTime
-      .getMinutes()
-      .toString()
-      .padStart(2, "0");
-    const formattedSeconds = cooldownEndTime
-      .getSeconds()
-      .toString()
-      .padStart(2, "0");
-
-    const countdownText = `${role.toUpperCase()}${formattedMinutes}:${formattedSeconds}`;
-
-    setTimers({ ...timers, [role]: countdownText });
-
-    // Copier dans le presse-papiers
-    navigator.clipboard
-      .writeText(countdownText)
-      .then(() =>
-        console.log("Texte copié dans le presse-papiers :", countdownText)
-      )
-      .catch((err) =>
-        console.error("Erreur lors de la copie dans le presse-papiers :", err)
-      );
-  };
+    return () => clearInterval(interval);
+  }, [gameTimer]);
 
   return (
-    <main className="flex flex-col justify-around items-center min-h-screen">
+    <main className="flex flex-col justify-center items-center gap-24 min-h-screen">
       <div className="w-full flex justify-around items-center">
         {leagueRoles.map((role, index) => (
-          <div key={index}>
+          <div
+            className="flex flex-col justify-center items-center gap-4"
+            key={index}
+          >
             <button
               className="transition-all hover:scale-110"
-              onClick={() => handleClick(role.name)}
+              onClick={() => startFlashCooldown(role.name)}
             >
               <Image
-                className="w-20 h-20"
-                width={200}
-                height={200}
+                className={cn("w-40 h-40", {
+                  "filter brightness-50": isSummonerIsTimed[role.name] === true,
+                })}
+                width={600}
+                height={600}
                 src={role.src}
                 alt={role.name}
               />
             </button>
-            {timers[role.name] && <p>{timers[role.name]}</p>}
+            {isSummonerIsTimed[role.name] && (
+              <p className="absolute text-2xl font-bold">
+                {cooldownTimers[role.name]}
+              </p>
+            )}
           </div>
         ))}
       </div>
-      <Button variant="outline" className="w-1/6" onClick={handleStart}>
-        Start
-      </Button>
+      {gameTimer ? (
+        <div>
+          <p className="text-lg font-bold">{`${Math.floor(elapsedTime / 60)}:${(
+            elapsedTime % 60
+          )
+            .toString()
+            .padStart(2, "0")}`}</p>
+        </div>
+      ) : (
+        <Button variant="outline" className="w-1/6" onClick={startGame}>
+          Start Game
+        </Button>
+      )}
     </main>
   );
 }
