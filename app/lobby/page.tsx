6297e74bbc1259c25cb74d78fc29fe0751e06a32
Teczer <mehdi.hattou1@gmail.com>
2024-03-28 15:33:17 +0100

    :lipstick: :sparkles: add routing + new future features

---
+++ app/lobby/page.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+import Link from "next/link";
+import { RxTrackPrevious } from "react-icons/rx";
+import { GoMoveToEnd } from "react-icons/go";
+
+export default function Home() {
+  return (
+    <main className="font-mono flex justify-center items-center gap-28 min-h-screen">
+      <Link className="fixed top-10 left-20" href={"/"}>
+        <Button variant="outline" size="icon">
+          <RxTrackPrevious className="h-4 w-4" />
+        </Button>
+      </Link>
+      <div className="flex flex-col justify-center items-center gap-8">
+        <h1 className="text-xl">Create a Lobby</h1>
+        <p className="text-sm">Choose a private room number</p>
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+        <Button variant="outline">Random Generate</Button>
+      </div>
+      <div className="w-[1px] h-[500px] bg-border"></div>
+      {/* JOIN LOBBY */}
+      <div className="flex flex-col justify-center items-center gap-8">
+        <h1 className="text-xl">Join a Lobby</h1>
+        <p className="text-sm">Type private room number</p>
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+        <div className="flex justify-center items-center gap-4">
+          <Input className="font-sans" type="text" placeholder="Paste Link" />
+          <Button variant="outline" size="icon">
+            <GoMoveToEnd className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+}
